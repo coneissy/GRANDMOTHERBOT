ca38af529d74adb64b26e1400a8a51a998e70059
coneissy <coneissyproworker@hotmail.com>
2026-09-24 15:15:23 +0300

    Build GrandMother research implementation: src/grandmother/__init__.py

---
+++ src/grandmother/__init__.py
@@ -0,0 +1,20 @@
+"""GrandMother research-faithful CEX-DEX arbitrage engine."""
+
+from .models import (
+    BuilderEconomics,
+    CandidateTransaction,
+    HedgeObservation,
+    MarkoutPoint,
+    PoolSwap,
+    SearcherPattern,
+    TokenInventory,
+)
+__all__ = [
+    "BuilderEconomics",
+    "CandidateTransaction",
+    "HedgeObservation",
+    "MarkoutPoint",
+    "PoolSwap",
+    "SearcherPattern",
+    "TokenInventory",
+]
