c76974c516ebcd788fbde2b9b17c0d141896b459
coneissy <coneissyproworker@hotmail.com>
2026-09-25 14:06:20 +0300

    Implement GrandMother CEX-DEX execution research layer

---
+++ src/grandmotherbot/__init__.py
@@ -0,0 +1,3 @@
+"""GrandMother venue-neutral CEX-DEX research engine."""
+from .models import ArbitrageEvent, ExecutionQuote, EconomicLedger
+__all__=["ArbitrageEvent","ExecutionQuote","EconomicLedger"]
